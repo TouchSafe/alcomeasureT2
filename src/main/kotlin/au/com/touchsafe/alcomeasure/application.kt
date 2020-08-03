529f5bca4881a44f3c7e8ceb97d48f4c12601648
Aidan Davies <adavies@byteback.com>
2020-08-03 18:21:30 +1000

    Added INFO log when email is sent

--- src/main/kotlin/au/com/touchsafe/alcomeasure/application.kt
+++ src/main/kotlin/au/com/touchsafe/alcomeasure/application.kt
@@ -24,7 +24,10 @@ fun main() {
 						AlcoMeasure.performTest(user)?.let { result ->
 							SqlServer.storeResult(connection, user, result)
 							val emailBody = java.text.MessageFormat.format(MESSAGES_BUNDLE.getString("EMAIL_BODY"), user.firstName, user.surname, "%.8f".format(result.value))
-							if (result.value != 0.0) Email.send(Email.TO, MESSAGES_BUNDLE.getString("EMAIL_SUBJECT"), emailBody, "photo1" to result.photo1Uri, "photo2" to result.photo2Uri, "photo3" to result.photo3Uri)
+							if (result.value != 0.0) {
+								Email.send(Email.TO, MESSAGES_BUNDLE.getString("EMAIL_SUBJECT"), emailBody, "photo1" to result.photo1Uri, "photo2" to result.photo2Uri, "photo3" to result.photo3Uri)
+								LOGGER.info("Sent email to ${Email.TO} with result value: ${"%.8f".format(result.value)} for ${user.firstName} ${user.surname}")
+							}
 						}
 					}
 				}

2bdd93eb8865c39fe8460df40f62b262e266b582
Aidan Davies <adavies@byteback.com>
2020-08-06 14:57:42 +1000

    Added ToggleableRollingFileAppender and replaced the RollingFileAppender in logback-deployment.xml with it

---
+++ src/main/kotlin/au/com/touchsafe/alcomeasure/ToggleableRollingFileAppender.kt
@@ -0,0 +1,25 @@
+package au.com.touchsafe.alcomeasure
+
+import ch.qos.logback.classic.spi.LoggingEvent
+import ch.qos.logback.core.rolling.RollingFileAppender
+
+// Toggleable ConsoleAppender, intending on using this for --debug flag
+// Need to use LoggingEvent explicitly in order to allow level setting
+open class ToggleableRollingFileAppender : RollingFileAppender<LoggingEvent>() {
+
+    companion object {
+        // Enabled by default
+        var enabled: Boolean = true
+    }
+
+    override fun subAppend(event: LoggingEvent) {
+        if (enabled) {
+            super.subAppend(event)
+        }
+    }
+
+    /*
+        Don't create a setEnabled function to allow logback.xml to use an <enabled> attribute,
+        prevents ability to set enabled variable programmatically
+    */
+}

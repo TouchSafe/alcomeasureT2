411e1ad89456d2aa26860938082f7c1148e183af
Aidan Davies <adavies@byteback.com>
2020-10-12 12:11:13 +1000

    Added working key press code in a coroutine

--- src/test/kotlin/au/com/touchsafe/alcomeasure/InputV2Test.kt
+++ src/test/kotlin/au/com/touchsafe/alcomeasure/InputV2Test.kt
@@ -1,5 +1,8 @@
 package au.com.touchsafe.alcomeasure
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.jnativehook.GlobalScreen
 import org.jnativehook.keyboard.NativeKeyEvent
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -12,13 +15,25 @@ class InputV2Test {
         GlobalScreen.registerNativeHook()
         GlobalScreen.addNativeKeyListener(InputV2.KEYBOARD_HOOK)
         val expectedRfid = Rfid(3, 1234)
-        // rawCode is not relevant for this test, and can change between OSes
-        GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_TYPED, 0, 0, NativeKeyEvent.VC_3, '3'))
-        GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_TYPED, 0, 0, NativeKeyEvent.VC_SEMICOLON, ';'))
-        GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_TYPED, 0, 0, NativeKeyEvent.VC_1, '1'))
-        GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_TYPED, 0, 0, NativeKeyEvent.VC_2, '2'))
-        GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_TYPED, 0, 0, NativeKeyEvent.VC_3, '3'))
-        GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_TYPED, 0, 0, NativeKeyEvent.VC_4, '4'))
+        // Launch key presses as a coroutine
+        GlobalScope.launch {
+            // Wait a couple of seconds before sending the keys
+            delay(2000L)
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED, 0, 0, NativeKeyEvent.VC_UNDEFINED, '3'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_RELEASED, 0, 0, NativeKeyEvent.VC_3, '3'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED, 0, 0, NativeKeyEvent.VC_SEMICOLON, ';'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_RELEASED, 0, 0, NativeKeyEvent.VC_SEMICOLON, ';'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED, 0, 0, NativeKeyEvent.VC_1, '1'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_RELEASED, 0, 0, NativeKeyEvent.VC_1, '1'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED, 0, 0, NativeKeyEvent.VC_2, '2'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_RELEASED, 0, 0, NativeKeyEvent.VC_2, '2'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED, 0, 0, NativeKeyEvent.VC_3, '3'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_RELEASED, 0, 0, NativeKeyEvent.VC_3, '3'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED, 0, 0, NativeKeyEvent.VC_4, '4'))
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_RELEASED, 0, 0, NativeKeyEvent.VC_4, '4'))
+            //TODO: This doesn't work. Need to either get NATIVE_KEY_TYPED events working, or find a way to do it with NATIVE_KEY_PRESSED - maybe if there's a KeyCode
+            GlobalScreen.postNativeEvent(NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_TYPED, 0, 0, NativeKeyEvent.VC_UNDEFINED, '\r'))
+        }
 
         val rfid = InputV2.getId()
 

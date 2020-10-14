fb35e800fbcf7a8fcf2c7d637d6572539da75923
Aidan Davies <adavies@byteback.com>
2020-10-14 14:31:15 +1000

    Added regex checking functions to RequiredSettingsProperties, and tests for them. Added regex checking function to application Changed validateAlcoMeasureDevice function to use alcomeasureLocationId setting from settings.properties

--- src/test/kotlin/au/com/touchsafe/alcomeasure/util/RequiredSettingsPropertiesTest.kt
+++ src/test/kotlin/au/com/touchsafe/alcomeasure/util/RequiredSettingsPropertiesTest.kt
@@ -46,7 +46,7 @@ internal class RequiredSettingsPropertiesTest {
         SETTINGS_PROPERTIES.setProperty("test2", "ghi")
         SETTINGS_PROPERTIES.setProperty("test3", "jkl")
 
-        assertTrue(requiredSettingsProperties.checkPresent())
+        assertTrue(requiredSettingsProperties.checkAllPresent())
     }
 
     @Test
@@ -54,7 +54,7 @@ internal class RequiredSettingsPropertiesTest {
         val requiredSettingsProperties = listOf<RequiredSettingsProperty>()
         SETTINGS_PROPERTIES.clear()
 
-        assertTrue(requiredSettingsProperties.checkPresent())
+        assertTrue(requiredSettingsProperties.checkAllPresent())
     }
 
     @Test
@@ -66,7 +66,7 @@ internal class RequiredSettingsPropertiesTest {
         SETTINGS_PROPERTIES.setProperty("test2", "ghi")
         SETTINGS_PROPERTIES.setProperty("test3", "jkl")
 
-        assertTrue(requiredSettingsProperties.checkPresent())
+        assertTrue(requiredSettingsProperties.checkAllPresent())
     }
 
     @Test
@@ -79,7 +79,7 @@ internal class RequiredSettingsPropertiesTest {
         )
         SETTINGS_PROPERTIES.clear()
 
-        assertFalse(requiredSettingsProperties.checkPresent())
+        assertFalse(requiredSettingsProperties.checkAllPresent())
     }
 
     @Test
@@ -94,7 +94,7 @@ internal class RequiredSettingsPropertiesTest {
         SETTINGS_PROPERTIES.setProperty("test2", "ghi")
         SETTINGS_PROPERTIES.setProperty("test3", "jkl")
 
-        assertTrue(requiredSettingsProperties.checkPresent())
+        assertTrue(requiredSettingsProperties.checkAllPresent())
     }
 
     @Test
@@ -109,7 +109,7 @@ internal class RequiredSettingsPropertiesTest {
         SETTINGS_PROPERTIES.setProperty("test", "abc")
         SETTINGS_PROPERTIES.setProperty("test1", "def")
 
-        assertFalse(requiredSettingsProperties.checkPresent())
+        assertFalse(requiredSettingsProperties.checkAllPresent())
     }
 
     @Test
@@ -122,7 +122,7 @@ internal class RequiredSettingsPropertiesTest {
         SETTINGS_PROPERTIES.setProperty("test2", "ghi")
         SETTINGS_PROPERTIES.setProperty("test3", "jkl")
 
-        assertFalse(requiredSettingsProperties.checkPresent())
+        assertFalse(requiredSettingsProperties.checkAllPresent())
     }
 
     @Test
@@ -136,6 +136,109 @@ internal class RequiredSettingsPropertiesTest {
         SETTINGS_PROPERTIES.setProperty("test2", "ghi")
         SETTINGS_PROPERTIES.setProperty("test3", "jkl")
 
-        assertFalse(requiredSettingsProperties.checkPresent())
+        assertFalse(requiredSettingsProperties.checkAllPresent())
+    }
+
+    @Test
+    fun testCheckMatchRegexNoRegex() {
+        SETTINGS_PROPERTIES.clear()
+        SETTINGS_PROPERTIES.setProperty("test", "abc")
+        SETTINGS_PROPERTIES.setProperty("test1", "def")
+        SETTINGS_PROPERTIES.setProperty("test2", "ghi")
+        SETTINGS_PROPERTIES.setProperty("test3", "jkl")
+
+        val requiredSettingsProperties = listOf(
+                RequiredSettingsProperty("test"),
+                RequiredSettingsProperty("test1"),
+                RequiredSettingsProperty("test2"),
+                RequiredSettingsProperty("test3"),
+        )
+
+        assertTrue(requiredSettingsProperties.checkAllMatchRegex())
+    }
+
+    @Test
+    fun testCheckMatchRegexAllMatch() {
+        SETTINGS_PROPERTIES.clear()
+        SETTINGS_PROPERTIES.setProperty("test", "abc")
+        SETTINGS_PROPERTIES.setProperty("test1", "def")
+        SETTINGS_PROPERTIES.setProperty("test2", "ghi")
+        SETTINGS_PROPERTIES.setProperty("test3", "jkl")
+
+        val requiredSettingsProperties = listOf(
+                RequiredSettingsProperty("test", regex = Regex("^abc$")),
+                RequiredSettingsProperty("test1", regex = Regex("^d.f$")),
+                RequiredSettingsProperty("test2", regex = Regex("^.+i$")),
+                RequiredSettingsProperty("test3", regex = Regex("^jkl.*$")),
+        )
+
+        assertTrue(requiredSettingsProperties.checkAllMatchRegex())
+    }
+    @Test
+    fun testCheckMatchRegexSomeMatch() {
+        SETTINGS_PROPERTIES.clear()
+        SETTINGS_PROPERTIES.setProperty("test", "abc")
+        SETTINGS_PROPERTIES.setProperty("test1", "def")
+        SETTINGS_PROPERTIES.setProperty("test2", "ghi")
+        SETTINGS_PROPERTIES.setProperty("test3", "jkl")
+
+        val requiredSettingsProperties = listOf(
+                RequiredSettingsProperty("test", regex = Regex("^abc$")),
+                RequiredSettingsProperty("test1", regex = Regex("^def$")),
+                RequiredSettingsProperty("test2", regex = Regex("^abc$")),
+                RequiredSettingsProperty("test3", regex = Regex("^abc$")),
+        )
+
+        assertFalse(requiredSettingsProperties.checkAllMatchRegex())
+    }
+    @Test
+    fun testCheckMatchRegexNoneMatch() {
+        SETTINGS_PROPERTIES.clear()
+        SETTINGS_PROPERTIES.setProperty("test", "abc")
+        SETTINGS_PROPERTIES.setProperty("test1", "def")
+        SETTINGS_PROPERTIES.setProperty("test2", "ghi")
+        SETTINGS_PROPERTIES.setProperty("test3", "jkl")
+
+        val requiredSettingsProperties = listOf(
+                RequiredSettingsProperty("test", regex = Regex("^z$")),
+                RequiredSettingsProperty("test1", regex = Regex("^zz$")),
+                RequiredSettingsProperty("test2", regex = Regex("^zzz$")),
+                RequiredSettingsProperty("test3", regex = Regex("^51q2z$")),
+        )
+
+        assertFalse(requiredSettingsProperties.checkAllMatchRegex())
+    }
+    @Test
+    fun testCheckMatchRegexEmptyRegex() {
+        SETTINGS_PROPERTIES.clear()
+        SETTINGS_PROPERTIES.setProperty("test", "abc")
+        SETTINGS_PROPERTIES.setProperty("test1", "def")
+        SETTINGS_PROPERTIES.setProperty("test2", "ghi")
+        SETTINGS_PROPERTIES.setProperty("test3", "jkl")
+
+        val requiredSettingsProperties = listOf(
+                RequiredSettingsProperty("test", regex = Regex("")),
+                RequiredSettingsProperty("test1", regex = Regex("")),
+                RequiredSettingsProperty("test2", regex = Regex("")),
+                RequiredSettingsProperty("test3", regex = Regex("")),
+        )
+
+        assertFalse(requiredSettingsProperties.checkAllMatchRegex())
+    }
+    @Test
+    fun testCheckMatchRegexUndefinedProperty() {
+        SETTINGS_PROPERTIES.clear()
+        SETTINGS_PROPERTIES.setProperty("test", "abc")
+        SETTINGS_PROPERTIES.setProperty("test1", "def")
+        SETTINGS_PROPERTIES.setProperty("test2", "ghi")
+
+        val requiredSettingsProperties = listOf(
+                RequiredSettingsProperty("test", regex = Regex("")),
+                RequiredSettingsProperty("test1", regex = Regex("")),
+                RequiredSettingsProperty("test2", regex = Regex("")),
+                RequiredSettingsProperty("test3", regex = Regex("")),
+        )
+
+        assertFalse(requiredSettingsProperties.checkAllMatchRegex())
     }
 }
